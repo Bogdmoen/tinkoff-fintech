b36d2f4a45533bceaa09f424a9c862b589f35e53
b.klimchuk <b.klimchuk@tinkoff.ru>
2022-03-16 13:47:48 +0300

    -

--- src/kt/TaskTwo.kt
+++ src/kt/TaskTwo.kt
@@ -4,19 +4,19 @@ fun main() {
     makeRoute(20, 4, arrayOf(1, 10, 15, 19))
 }
 
-fun makeRoute(perimeter: Int, villageCount: Int, distance: Array<Int>) {
+fun makeRoute(perimeter: Int, villageCount: Int, distances: Array<Int>) {
 
-    val distanceToFirst = (perimeter - distance[distance.lastIndex]) + distance[0]
+    val distanceToFirst = (perimeter - distances[distances.lastIndex]) + distances[0]
     val result:Int
     var tmpClockWise = 0
     var tmpCounterClockWise = distanceToFirst
 
-    for(i in 0 until distance.size - 1) {
-        tmpClockWise += distance[i + 1] - distance[i]
+    for(i in 0 until distances.size - 1) {
+        tmpClockWise += distances[i + 1] - distances[i]
     }
 
-    for(i in 1 until distance.size - 1) {
-        tmpCounterClockWise += distance[i + 1] - distance[i]
+    for(i in 1 until distances.size - 1) {
+        tmpCounterClockWise += distances[i + 1] - distances[i]
     }
 
     result = tmpClockWise.coerceAtMost(tmpCounterClockWise)
