18f75475132a72669d3121c64963e53899bc9d89
b.klimchuk <b.klimchuk@tinkoff.ru>
2022-03-22 12:55:28 +0300

    taskThree added

---
+++ src/kt/TaskThree.kt
@@ -0,0 +1,35 @@
+package kt
+
+import java.math.RoundingMode
+import kotlin.random.Random
+
+fun main() {
+
+    cutLogs(5, listOf(15, 11, 19))
+}
+
+fun cutLogs(cuts: Int, logs: List<Int>) {
+
+    var logsDouble = logs.map{it.toDouble()}.toMutableList()
+
+    val result: Int
+    var cutOne = 0.0
+    var cutTwo = 0.0
+
+    for (i in cuts downTo 0) {
+        logsDouble = logsDouble.sortedDescending().toMutableList()
+        if (logsDouble[0] <= 0.1) {
+            break
+        }
+        cutOne = Random.nextDouble(0.1, logsDouble[0])
+        cutTwo = logs[0] - cutOne
+
+        logsDouble = logsDouble.drop(1).toMutableList()
+        logsDouble.add(cutOne)
+        logsDouble.add(cutTwo)
+
+    }
+
+    result = cutOne.coerceAtMost(cutTwo).toBigDecimal().setScale(0, RoundingMode.UP).toInt()
+    println(result)
+}
